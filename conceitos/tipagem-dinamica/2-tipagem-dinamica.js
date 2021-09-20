ad674e1e9cd721f6cff5477a6752c97d3c90a375
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-09-20 15:28:58 -0300

    Exemplo de tipagem dinâmica com JavaScript

---
+++ conceitos/tipagem-dinamica/2-tipagem-dinamica.js
@@ -0,0 +1,8 @@
+var minhaVariavel = 30;
+
+minhaVariavel = "Texto";
+
+console.log(minhaVariavel);
+
+//Na tipagem dinâmica não é preciso explicitar o tipo da variavel no momento da sua criação
+//Pode mudar o tipo apenas inserindo outro tipo de dado como uma string
