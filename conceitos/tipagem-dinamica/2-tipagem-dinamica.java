9a51e27f2b148fc81e25eb9e1ca5757839fc89cb
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-09-20 15:46:26 -0300

    Exemplo de tipagem explícita, estática no JAVA

---
+++ conceitos/tipagem-dinamica/2-tipagem-dinamica.java
@@ -0,0 +1,10 @@
+public class TipagemDinamica {
+    public static void main(String[] args) {
+        int meuNumero = 10;
+        meuNumero = "Texto";
+
+        System.out.println(meuNumero);
+    }
+}
+
+//Na tipagem estática, explícita, não se pode mudar o tipo de dado dando outro valor a ele
