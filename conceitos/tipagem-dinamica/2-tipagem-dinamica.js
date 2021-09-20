1aec8e0ec851d223fcf25b8f534ab500d731b844
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-09-20 15:35:24 -0300

    Adição de comentário no exemplo de tipagem dinâmica do JavaScript

--- conceitos/tipagem-dinamica/2-tipagem-dinamica.js
+++ conceitos/tipagem-dinamica/2-tipagem-dinamica.js
@@ -6,3 +6,4 @@ console.log(minhaVariavel);
 
 //Na tipagem dinâmica não é preciso explicitar o tipo da variavel no momento da sua criação
 //Pode mudar o tipo apenas inserindo outro tipo de dado como uma string
+//Inferência de tipo é quando você define um valor para uma variável é que o computador vai saber esse tipo
