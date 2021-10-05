e5cfc2aa5df68e75fc2059f0d02546c3436db0a6
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-05 13:38:20 -0300

    Método replace() de String

--- tipos/string/1-string.js
+++ tipos/string/1-string.js
@@ -6,3 +6,7 @@ console.log(`Quantidade de letras: ${textSize}`);
 const splittedText = 'Texto'.split('x');
 console.log(`\nArray com as posições separadas pelo delimitador:`, splittedText);
 
+//Busca um valor e substitui por outro
+const replacedText = 'Texto'.replace('Text', 'txeT');
+console.log(`\nSubstituição de valor: `, replacedText);
+
