1dce1d5d974a4064255b52a22c7779d6e638b9d5
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-07 22:30:18 -0300

    Método toFixed() de Number

--- tipos/number/1-number.js
+++ tipos/number/1-number.js
@@ -3,3 +3,7 @@ const myNumber = 12.4032;
 //Transformar número para string
 const numberAsString = myNumber.toString();
 console.log('Número transformado em string: ', typeof numberAsString);
+
+//Retorna número com um número de casas decimais
+const fixedTwoDecimals = myNumber.toFixed(2);
+console.log('\nNúmero com duas casas decimais: ', fixedTwoDecimals);
