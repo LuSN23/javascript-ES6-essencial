d99c0c699d6cd60858261e41ba1e05190177a7ff
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-07 22:33:41 -0300

    Método parseFloat() de Number

--- tipos/number/1-number.js
+++ tipos/number/1-number.js
@@ -7,3 +7,6 @@ console.log('Número transformado em string: ', typeof numberAsString);
 //Retorna número com um número de casas decimais
 const fixedTwoDecimals = myNumber.toFixed(2);
 console.log('\nNúmero com duas casas decimais: ', fixedTwoDecimals);
+
+//Transforma uma string em float(do tipo Number)
+console.log('\nString parseada para float: ', parseFloat('13.22'));
