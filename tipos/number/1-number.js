6de5c98e4c2e8ce2b147328bfb3439083c32e619
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-07 22:40:57 -0300

    Adição de informações nos comentários de parseFloat()

--- tipos/number/1-number.js
+++ tipos/number/1-number.js
@@ -10,6 +10,8 @@ console.log('\nNúmero com duas casas decimais: ', fixedTwoDecimals);
 
 //Transforma uma string em float(do tipo Number)
 console.log('\nString parseada para float: ', parseFloat('13.22'));
+//Se o último valor fosse 0, como em 13.20, ia ficar 13.2, se eu quiser que o 0 apareça, uso o toFixed()
+
 
 //Transforma uma string em int(do tipo Number)
 console.log('\nString parseada para int:', parseInt('13.20'));
