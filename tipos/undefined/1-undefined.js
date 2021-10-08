dcc0cddb5589326712b90f024b201b469164eb62
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-07 22:48:58 -0300

    Tipo undefined

---
+++ tipos/undefined/1-undefined.js
@@ -0,0 +1,5 @@
+let undefinedVariable;
+
+console.log('Tipo da variável: ', typeof undefinedVariable);
+
+//typeof ajuda a ver se uma variável não foi definida, já que undefined é um tipo específico
