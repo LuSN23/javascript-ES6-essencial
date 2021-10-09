601089f7efc2f18f8796e4ec71f204defa87ca39
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-09 15:00:58 -0300

    Propriedades Symbol em listagem de enumerables com for...in

--- tipos/symbol/1-symbol.js
+++ tipos/symbol/1-symbol.js
@@ -21,3 +21,25 @@ const user = {
 }
 
 console.log(user);
+
+
+//Symbols criam propriedades que não são enumerables
+for (const key in user) {
+    if (user.hasOwnProperty(key)){
+        console.log(`\nValor da chave = ${key}: ${user[key]}`);
+    }
+}
+/*
+key = nome de constante genérico para comportar o valor do resultado do nome das 
+propridades.
+hasOwnProperty() retorna um valor boolean indicando se o objeto tem a propriedade 
+específica(que não seja herdada mas própria)
+*/
+
+/*ENUMERABLES: São as propriedades que tem a flag interna setada como true, o 
+que é default para propriedades criadas a partir de simples atribuição ou via 
+um inicializador de propriedade*/ 
+/*A função for...in mostra todas as propriedades que são enumerables, então o symbol
+não aparecerá na saída. Mostra um pouco do modificador de acesso private, porque não 
+dá para acessar o symbol com estruturas padrão do JavaScript como as de repetição. Há 
+no entanto, outras maneiras de fazer isso*/
