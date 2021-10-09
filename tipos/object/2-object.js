44b2d92f1a253e425849421d3f2681ef0c0f12f0
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-09 11:35:09 -0300

    Método Object.entries()

--- tipos/object/2-object.js
+++ tipos/object/2-object.js
@@ -8,3 +8,7 @@ console.log('Propriedades do objeto user:', Object.keys(user));
 
 //Recupera os valores das chaves do objeto
 console.log('\nValores das propriedades do objeto user:', Object.values(user));
+
+//Retorna um array de arrays contendo [ nome_prop, valor_prop ]
+console.log('\nLista de propriedades e valores:', Object.entries(user));
+//*Experimental, antes de usar verificar a compatibilidade com o navegador
