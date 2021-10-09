40a5b1b837158ad56e5c49560e0841ba36c78bbc
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-09 11:27:47 -0300

    Método Object.values()

--- tipos/object/2-object.js
+++ tipos/object/2-object.js
@@ -5,3 +5,6 @@ const user = {
 
 //Recupera as chaves do objeto
 console.log('Propriedades do objeto user:', Object.keys(user));
+
+//Recupera os valores das chaves do objeto
+console.log('\nValores das propriedades do objeto user:', Object.values(user));
