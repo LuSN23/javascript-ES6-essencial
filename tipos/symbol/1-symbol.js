6962f2098643d4e144b1bd7352604c88cf59b221
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-09 14:13:25 -0300

    Evitando sobrescrever propriedades com Symbol

--- tipos/symbol/1-symbol.js
+++ tipos/symbol/1-symbol.js
@@ -9,3 +9,15 @@ de objetos ou classes, mas como o JavaScript não tem o modificador de acesso(pr
 protected) algumas pessoas utilizam a mesma convensão do Python de colocar o _ na frente, porém 
 não faz parte da documentação, ele acabou por ser mais um atributo único mesmo*/
 
+//Previnir conflito entre nomes de propriedades
+//Perceba que a inicialização deles são iguais
+const nameSymbol1 = Symbol('name');
+const nameSymbol2 = Symbol('name');
+//Evita-se sobrescrever as propriedades porque cada um é único, ele habilita uma forma de fazer isso(ver o OUTPUT)
+const user = {
+    [nameSymbol1]: 'Luciana',
+    [nameSymbol2]: 'Outro nome',
+    lastName: 'Sarai Nunes'
+}
+
+console.log(user);
