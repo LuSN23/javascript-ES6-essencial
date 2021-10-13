8ee1b3dc7dc1aa07e06f60fa98c5394011b832ce
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-13 20:32:34 -0300

    Exemplos de Arrow Functions com return implícito e explícito

--- tipos/functions/1-functions.js
+++ tipos/functions/1-functions.js
@@ -2,3 +2,12 @@
 function fn() {
     return 'Code here';
 }
+
+//Arrow function
+const arrowFn = () => 'Code here'; //return implicito
+const arrowFn2 = () => {
+    //No caso de mais de uma expressão é exigido o return explícito
+    //Mais de uma expressão
+    return 'Code here';
+}
+
