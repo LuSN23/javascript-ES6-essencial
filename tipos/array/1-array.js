3ebf826a23d6c0164645cc49fdaf71725312b24c
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-15 23:47:05 -0300

    Propriedade length de Array

---
+++ tipos/array/1-array.js
@@ -0,0 +1,27 @@
+const users = ['Luciana', 'Pedro', 'Jennifer'];
+
+const gender = {
+    MAN: Symbol('M'),
+    WOMAN: Symbol('W')
+}
+
+const persons = [
+    {
+     name: 'Luciana',
+     age: 28,
+     gender: gender.WOMAN
+    },
+    {
+     name: 'Pedro',
+     age: 43,
+     gender: gender.MAN
+    },
+    {
+     name: 'Jennifer',
+     age: 18,
+     gender: gender.WOMAN
+    }
+];
+
+//Retornar a quantidade de itens de um array
+console.log('Items: ', persons.length);
