2c3a226d3705e453a29684c1f63b3be16bddd54f
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-17 00:29:50 -0300

    Não igual/diferente - Operadores de comparação

--- operadores/comparacao/1-comparacao.js
+++ operadores/comparacao/1-comparacao.js
@@ -3,3 +3,7 @@
 "3" == var1
 3 == '3' //Retorna verdadeiro, valor igual mas tipo diferente.
 //linters geralmente são configurados para não permitir esse tipo de comparação 3 == '3' como true
+
+//Não Igual/diferente (!==)
+//Retorna verdadeiro caso os operandos não sejam iguais
+var2 != "3"
