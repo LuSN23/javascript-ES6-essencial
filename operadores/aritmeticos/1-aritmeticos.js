6f2aa8023dbb990a164de8379999942e200af588
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-16 23:48:06 -0300

    Módulo - Operadores Aritméticos

---
+++ operadores/aritmeticos/1-aritmeticos.js
@@ -0,0 +1,19 @@
+/*
+//Operador binário
+operando1 operador operando2
+
+1 + 2
+
+
+//Operador unário
+operando1 operador
+operador operando1
+
+x++
+++x
+*/
+
+//Módulo (%)
+//Operador binário. Retorna o inteiro restante da divisão dos dois operandos.
+
+12 % 5 //Retorna 2
