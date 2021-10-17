3ecb634fee88d2922ea07a5d118cd6b6890b5772
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-17 00:25:41 -0300

    Igual - Operadores de comparação

---
+++ operadores/comparacao/1-comparacao.js
@@ -0,0 +1,5 @@
+// Igual (==)
+//Retorna verdadeiro caso os operandos sejam iguais. 
+"3" == var1
+3 == '3' //Retorna verdadeiro, valor igual mas tipo diferente.
+//linters geralmente são configurados para não permitir esse tipo de comparação 3 == '3' como true
