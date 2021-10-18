73684d71c22bb0130bc8c1f0bdde3768e244d6ff
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-18 14:33:41 -0300

    Operador Ternário - Operador condicional

---
+++ operadores/condicional/1-condicional.js
@@ -0,0 +1,6 @@
+//Ternário
+condicao ? valor1 : valor2
+//Se a condição é satisfeita o valor recebido é o valor1 senão o valor2
+
+true ? 'foo' : 'bar'   //Retorna 'foo'
+false ? 'foo' : 'bar'  //Retorna 'bar'
