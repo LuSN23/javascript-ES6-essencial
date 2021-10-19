8734d43f1068f507fdb0857b33eae5d20723d7ab
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-19 14:48:30 -0300

    Operador in em objetos predefinidos - Outros operadores(binários)

--- operadores/outros/2-binarios.js
+++ operadores/outros/2-binarios.js
@@ -10,3 +10,11 @@ var arvores = new Array("pau-brasil", "loureiro", "cedro", "carvalho", "sicômor
 "cedro" in arvores;   //retorna false(você deve especificar o número do índice, não o valor daquele índice)
 //"cedro" in arvores[2]; (Aí sim retornaria true)
 "length" in arvores;  //retorna true (length é uma propriedade de array) (string e array tem a propriedade length)
+
+//Objetos predefinidos
+"PI" in Math;            //retorna true
+//Math é um objeto global, não é um objeto construtor, possui propriedades e métodos estáticos
+var minhaString = new String("coral"); //nesse caso é um objeto contrutor(String)
+"length" in minhaString; //retorna true
+
+
