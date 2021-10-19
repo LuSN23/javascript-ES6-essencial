7bfd31a3ae0fa1a02df53bb54ee78c4ddb8c2134
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-18 23:42:41 -0300

    OR - Operadores lógicos

--- operadores/logicos/1-logicos.js
+++ operadores/logicos/1-logicos.js
@@ -15,3 +15,24 @@ ou
 o primeiro false no caso de 2 false. 
 No caso de 2 trues como 2 strings true, retorna a 
 segunda string true. */
+
+//OR lógico (||)
+exp1 || exp2
+
+var o1 = true || true;      //t || t retorna true
+var o2 = false || true;     //f || t retorna true
+var o3 = true || false;     //t || f retorna true
+var o4 = false || (3 == 4); //f || f retorna false
+var o5 = "Gato" || "Cão";   //t || t retorna Gato
+var o6 = false || "Gato";   //f || t retorna Gato
+var o7 = "Gato" || false;   //t || f retorna Gato
+
+/* *ATENÇÃO ao valor que vai ser colocado dentro da variável, 
+no ||/OR retorna o true se houver apenas 1 true,
+ou
+o primeiro true no caso de 2 true.
+No caso de 2 true como 2 strings true, retorna a primeira string true.
+Muito usado na atribuição de variáveis:
+conteudo = conteudo || 'fallback'; (se conteudo for undefined retorna o fallback) 
+fallback = Plano B para alguma ação do programador */
+
