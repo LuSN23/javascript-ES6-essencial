2ba78e7109721bc8187e1502b20ef4eebabce7e4
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-25 00:01:23 -0300

    for...of - Estruturas de Repetição

--- repeticao/for...in-of/1-for-in-of.js
+++ repeticao/for...in-of/1-for-in-of.js
@@ -4,5 +4,14 @@ arr.foo = "hello"; //função e array são objetos então é possível adicionar
 for(let i in arr) {
     console.log(i); //"0", "1", "2", "foo"
 }
-//Uma propriedade enumerada diferente é atribuída a cada iteração
+//Uma propriedade enumerada diferente é atribuída a cada iteração, retorna indíces e chaves
 //Retorna além de indíces
+
+for (let i of arr) {
+    console.log(i); //"3", "5", "7" 
+}
+//Uma propriedade iterável diferente é atribuída a cada iteração, retorna o valor dos índices e não de chaves
+//Retorna o conteúdo dos índices e apenas conteúdos do array
+
+//for...in = Índices e chaves
+//for...of = Valores dos índices do array apenas
