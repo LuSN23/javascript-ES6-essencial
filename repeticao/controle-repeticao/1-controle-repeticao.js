159b235a1666af1a5d35b4f0fe22e4629a8cf84e
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-25 00:18:46 -0300

    Controle com o continue - Estruturas de Repetição

--- repeticao/controle-repeticao/1-controle-repeticao.js
+++ repeticao/controle-repeticao/1-controle-repeticao.js
@@ -9,5 +9,21 @@ while(true) { //O break tem poder de cancelar esse true
     if(index > 2) {
         break;  //Sai do laço de repetição/interrompe a execução
     }
-    console.log(index);
+    console.log(index); //1 2
 }
+
+//continue
+console.log('\nExemplo de utilização de continue:');
+const array = [1, 2, 3, 4, 5, 6];
+
+for(let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+    if(element % 2 === 0) {
+        continue; //Se o valor do índice do array for divisível por 2, pule para o próximo(não exibindo ele)
+    }
+    console.log(element); //1 3 5
+}
+
+//break: Finaliza a execução, sai do laço
+//continue: Pula o elemento que satisfaz a condição/o ignora
