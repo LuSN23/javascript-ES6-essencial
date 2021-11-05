5837c7525290866f8f2bf474682ab58e5ddf3b72
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-04 23:26:32 -0300

    Exemplo 9: Situações mais complicadas com this - Ajustando this em método callback - Original - POO

--- POO/09.js
+++ POO/09.js
@@ -38,3 +38,32 @@ person.showFullName.apply(anotherPerson); //Rohit Khan
 
 //Então, o contexto é agora "anotherPerson" porque "anotherPerson" invocou 
 //person.showFullName() ao usar apply().
+
+/*
+//Ajustando this quando usado em um método callback:
+CALLBACK: Uma rotina(execução de uma tarefa específica) que é passada como parâmetro 
+para outro método.
+- No código abaixo, o objeto que invoca(durante o evento) clickHandler() é o 'button' 
+e então o seu contexto(o this) não mais refere-se ao objeto 'user'.
+- Chama-se o user.clickHandler() já que clickHandler é um método definido no 'user'.
+- O contexto muda quando se executa um método em algum outro objeto do qual o objeto 
+foi originalmente definido, e passa a ser o objeto que invoca o método.*/
+//EXEMPLO 03: Original:
+//Tem-se um objeto simples com o método "clickHandler" para ser usado quando um 
+//botão na página é clicado.
+var user = {
+    data: [
+        {name: "T. Woods", age: 37},
+        {name: "P. Mickelson", age: 43}
+    ],
+    clickHandler: function(event){
+        // Número aleatório entre 0 e 1
+        var randomNum = ((Math.random() * 2 | 0) + 1) - 1;
+
+        // Mostra um nome e idade aleatórios do array "data"
+        console.log(this.data[randomNum].name + ' ' + this.data[randomNum].age);
+    }
+}
+//O botão é envolvido por um wrapper de jQuery ($), então ele agora é um objeto 
+//jQuery a saída será "undefined" porque não há propriedade em "data" no objeto-botão.
+$('button').on('click', user.clickHandler); //"Cannot read property '0' of undefined"
