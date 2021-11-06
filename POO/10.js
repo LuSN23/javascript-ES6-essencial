fa3c8c6088880438c48e757cf1619d07b01ddd95
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-06 14:10:21 -0300

    Exemplo 10: Exemplo otimizado de herança, modificando métodos no prototype - POO

--- POO/10.js
+++ POO/10.js
@@ -29,3 +29,41 @@ Animal se movimenta e todo Cachorro late).
 movimentar() e latir() serão criadas novamente, já que todos irão movimentar-se e 
 latir da mesma forma, então o código não está muito preciso/otimizado.
 */
+
+//EXEMPLO OTIMIZADO:
+'use strict';
+
+function Animal() {}
+
+Animal.prototype.qtdePatas = 0;
+Animal.prototype.movimentar = function() {}
+
+function Cachorro(morde) {
+    this.qtdePatas = 4;
+    this.morde = morde;
+}
+
+Cachorro.prototype = Object.create(Animal);//Inserindo em prototype de Cachorro que 
+//ele deriva do objeto Animal, e por isso herda o que está nele
+Cachorro.prototype.latir = function() { //Inserindo novo método no prototype
+    console.log('Au! au!');             //de Cachorro
+}
+
+const pug = new Cachorro(false);
+const pitbull = new Cachorro(true);
+
+/*
+Object.create() = Cria um novo objeto, utilizando um outro objeto existente como
+protótipo para o novo objeto a ser criado.
+- create() é um método de Object
+- Quando crio a minha função construtora, ela permite que eu escreva no prototype(
+que contém a definição do meu objeto).
+- Nesse caso eu estou escrevendo que o prototype de Cachorro é derivado do prototype 
+de Animal e inserindo um novo método que em Cachorro que é latir().
+- Então essas funções movimentar() e latir(), não serão criadas novamente, elas já 
+estarão declaradas nos prototypes.
+- Posso inserir novas propriedades e métodos no meu prototype e todas as instâncias 
+as terão atualizadas. ATENÇÃO: *(Isso, no entanto, é bem perigoso, porque é possível alterar 
+implementação de tipos nativos como o String e bagunçar métodos já prontos, não deve-se 
+alterar tipos já existentes).
+*/
