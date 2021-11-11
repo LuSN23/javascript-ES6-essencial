af2f0777a1a531bfaae2232d52c9458d1ae60cc2
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-11 00:13:04 -0300

    Exemplo 14: Encapsulamento - Definindo as novas palavras chave get e set para um atributo dentro de constructor usando classe - POO

--- POO/14.js
+++ POO/14.js
@@ -1,23 +1,51 @@
 //Encapsulamento
-//Versão mais antiga:
+//Versão mais antiga (não usando classe):
 'use strict';
 
 function Person(initialName) {
     var name = initialName;
 
-    Object.definePropriety(this, 'name', {
+    Object.defineProperty(this, 'name', {
         get: function() {
             return name;
-        },
+        },                          //Aqui as definições dos métodos get e set
         set: function(value) {
             name: value;
         }
     });
 }
+/*defineProperty() é um método estático que define uma nova propriedade diretamente em um objeto, ou 
+modifica uma propriedade existente e retorna o objeto. (Você invoca esse método diretamente no constructor
+do Object ao invés de invocar em uma instância do tipo Object)
+this = onde é definido o objeto no qual será definida a propriedade
+'name' = onde o nome da propriedade que será definida ou modificada
+*/
 
+//Versão mais nova: (usando classe):
+'use strict';
 
+class Person {
+    #name = '';
 
+    constructor(name) {
+        this.#name = name;
+    }
 
+    get name() {            
+        return this.#name;
+    }
+
+    set name() {
+        this.#name = name;
+    }
+}
+
+/*
+Em versões com suporte a classe:
+- Conseguimos agora ter as palavras get e set para determinado atributo.
+- Agora quando eu faço const p = new Person; eu consigo dar um p.name e p.name = 'Thiago', executando 
+esses métodos que estão dentro do constructor direto.
+*/
 
 
 /*
