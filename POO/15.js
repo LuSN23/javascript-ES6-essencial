c3caf7291cd67882cf1b23f36d31a0f2abd24244
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-11 00:31:56 -0300

    Exemplo 15: Static - Criando método estático usando classe e a palavra-chave static - POO

--- POO/15.js
+++ POO/15.js
@@ -10,6 +10,23 @@ Person.walk = function() {     //Como Person é function e functions são object
 }
 
 console.log(Person.walk()); //"walking..." //Chamando walk() sem instanciar um novo objeto Person
+
+//Maneira mais nova (usando classe):
+'use strict '
+class Person {
+    static walk(){
+        console.log('walking...');
+    }
+}
+
+console.log(Person.walk()); //"walking..."
+
 /*
-Permite acessar métodos/atributos sem instanciar uma determinada classe 
+- A palavra chave static define um método estático ou propriedade de uma classe(nem métodos nem 
+propriedades estáticas podem ser chamadas em instâncias da classe, eles são chamados na classe 
+em si mesma).
+- Métodos estáticos são muitas das vezes funções de utilidade, como funções para criar ou clonar 
+objetos, enquanto propriedades estáticas são úteis para caches, configuração corrigidas, ou qualquer 
+outro dado que você não precise replicar pelas instâncias. 
+- Permite acessar métodos/atributos sem instanciar uma determinada classe 
 */
