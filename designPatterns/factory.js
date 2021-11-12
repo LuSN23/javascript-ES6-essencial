184f0636e75b73c8bbd52497f692bdc416cdf5ec
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-12 04:54:49 -0300

    Factory: Outros exemplos - 01

--- designPatterns/factory.js
+++ designPatterns/factory.js
@@ -22,3 +22,15 @@ const user = FakeUser();
 Todas as funções que retornam um objeto, sem a necessidade de chamá-las com o new, são consideradas 
 funções Factory(fábrica).
 */
+
+//Outros exemplos:
+//01:
+function Pessoa() {
+    return {
+        name: 'Luciana',
+        lastName: 'Sarai Nunes'
+    }
+}
+
+const p = Pessoa();
+console.log(p);
