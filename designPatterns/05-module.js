cc3daf7df6cc811efba3874b792146ecfe944720
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-16 21:12:13 -0300

    Module: Exemplo 01 de depois do ES6

--- designPatterns/05-module.js
+++ designPatterns/05-module.js
@@ -15,3 +15,16 @@ Criava-se uma if, que é uma função de execução imediata, passava o que ela 
 fazia as alterações(//Your plugin here). 
 */
 
+//Com o ES6:
+class Person {
+    constructor(name) {        //Declare uma classe, função ou objeto
+        this.name = name;
+    }
+}
+
+export default Person;      //Exporte ela
+
+//Import Person
+import Person from './models/person'; //E a importação do que a gente precisar
+//Separação dos arquivos de acordo com a responsabilidade(modular)
+//*Não é possível usar a implementação import, export no node, apenas no Babel
