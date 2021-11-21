9b47503f9d7deca20cfd5d304241ac57b55f3b9e
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-21 18:17:31 -0300

    Criação básica de arrays: Simples e instanciando um novo array

---
+++ arrays-manipulacao_e_iteracao/01-criacao_de_arrays.js
@@ -0,0 +1,5 @@
+//Criar um array
+
+//Criações básicas:
+const arr = [1, 2, 3]; //Forma mais simples de criação de um array
+const arr2 = new Array(1, 2, 3); //Instanciando do objeto construtor Array
