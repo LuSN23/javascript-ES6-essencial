838ea7c7cac09506df51870a971af9b1c54de1d6
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-22 22:53:48 -0300

    Arrays: Usando o Array()

--- arrays-manipulacao_e_iteracao/01-criacao_de_arrays.js
+++ arrays-manipulacao_e_iteracao/01-criacao_de_arrays.js
@@ -4,6 +4,8 @@
 const arr = [1, 2, 3]; //Forma mais simples de criação de um array
 const arr2 = new Array(1, 2, 3); //Instanciando do objeto construtor Array
 
+
+
 //Método Array.of()
 /*
 Definição:
@@ -20,3 +22,23 @@ people //(3) ["John", "Chris", "Jenny"]
 //Exemplo 03: (No browser)
 const numbersAndStrings = Array.of(1, 2, 'strings', 'texto'); //Aceita dados diferentes no mesmo array
 numberAndStrings //(4) [1, 2, "strings", "texto"]
+
+
+
+// Usando o Array
+/*
+Definição:
+Cria uma nova instância de array de acordo com os parâmetros informados.
+*/
+//Exemplo 01:
+const arr = Array(3); //Apenas com um integer, cria o número de items, mas vazios
+// [empty x 3]
+const arr2 = Array(3, 2); //Será criado de maneira semelhante ao Array.of()
+// [3, 2]
+//Exemplo 02: (No browser)
+const arrWith3Positions = Array(3);
+arrWith3Positions //(3) [empty x 3]
+//Exemplo 03: (No browser)
+const peopleList = Array('John', 'Chris', 'Jenny');
+peopleList //(3) ["John", "Chris", "Jenny"]
+
