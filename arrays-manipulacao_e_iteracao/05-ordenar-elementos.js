6c0517b0323d9df37243b15601d341e6a77309b4
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-27 20:47:12 -0300

    Arrays: Método sort() - Ordenação

---
+++ arrays-manipulacao_e_iteracao/05-ordenar-elementos.js
@@ -0,0 +1,45 @@
+//Ordenar elementos:
+
+//Método sort()
+/*
+Ordena os elementos de um array de acordo com a condição(tem que ser uma comparação?).
+- Recebe o item atual(current) e o próximo item(next) como parâmetros.
+- sort = classificar
+- Retorna o array ordenado.
+- O modo default é o ordenado de maneira crescente
+*/
+//Exemplo 01: (No browser)
+const students = [
+    {name: 'John', grade: 7},
+    {name: 'Jenny', grade: 5},
+    {name: 'Peter', grade: 4}
+];
+
+//Supondo que quero ordenar por notas em ordem crescente
+students.sort((current, next) => current.grade - next.grade);
+//Outro exemplo: firstItem.grade - secondItem.grade
+/*
+(3) [{...}, {...}, {...}]
+    0: {name: "Peter", grade: 4}
+    1: {name: "Jenny", grade: 5}
+    2: {name: "John", grade: 7}
+      length: 3
+      __proto__: Array(0)
+*/
+
+//Ou por ordem decrescente
+students.sort((current, next) => next.grade - current.grade); 
+/*
+(3) [{...}, {...}, {...}]
+    0: {name: "John", grade: 7}
+    1: {name: "Jenny", grade: 5}
+    2: {name: "Peter", grade: 4}
+      length: 3
+      __proto__: Array(0)
+*/
+
+//Exemplo 02:
+//Um sort() simples
+const arr = [1, 3, 2, 5, 4];
+
+arr.sort(); // [1, 2, 3, 4, 5]  //Apenas ordena os valores em ordem crescente
