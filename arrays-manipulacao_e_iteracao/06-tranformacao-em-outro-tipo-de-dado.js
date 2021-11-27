8d377be03dcc45912a01516673c1986caf980bfe
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-27 20:58:56 -0300

    Arrays: Método join() - Transformação - array para string

---
+++ arrays-manipulacao_e_iteracao/06-tranformacao-em-outro-tipo-de-dado.js
@@ -0,0 +1,11 @@
+//Transformando em outro tipo de dado
+
+//Método join()
+/*
+Junta todos os elementos de um array, separados por um delimitador e retorna uma string.
+- Tranforma array em string.
+*/
+//Exemplo 01:
+const arr = [1, 2, 3, 4, 5];
+
+arr.join('-'); // "1-2-3-4-5"
